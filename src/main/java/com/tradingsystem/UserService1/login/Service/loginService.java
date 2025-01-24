dcfa9fb40b311f9099edb40e67baa2af6931d1b6
Supriniiii <khensanimnisi91@gmail.com>
2025-01-24 10:09:23 +0100

    sign up and Login code

--- src/main/java/com/tradingsystem/UserService1/login/Service/loginService.java
+++ src/main/java/com/tradingsystem/UserService1/login/Service/loginService.java
@@ -1,25 +1,46 @@
-package com.tradingsystem.UserService1.login.Service;
+package com.tradingsystem.UserService1.Login.Service;
 
-import lombok.Data;
+import com.tradingsystem.UserService1.Exceptions.TraderNotFoundException;
+import com.tradingsystem.UserService1.Model.Trader;
+import com.tradingsystem.UserService1.Repository.TraderRepository;
+import com.tradingsystem.UserService1.TraderDTO.TraderDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Data
-public class loginService {
- private String a;
- private String b;
-
-    public String getA() {
-        return a;
+@Service
+public class LoginService implements LoginServiceRepository {
+private TraderRepository traderRepository;
+    @Autowired
+    public LoginService(TraderRepository traderRepository) {
+        this.traderRepository = traderRepository;
     }
-
-    public void setA(String a) {
-        this.a = a;
+    @Override
+    public TraderDTO getTraderByEmail(String email) {
+        Trader trader=traderRepository.findByEmail(email).orElseThrow(()-> new TraderNotFoundException("User is not found"));
+        return mapToDTO(trader);
     }
+    //MAPPING THE DTO TO THE ENTITY
+    public TraderDTO mapToDTO(Trader trader){
+        TraderDTO traderDTO=new TraderDTO();
+        traderDTO.setId(trader.getId());
+        traderDTO.setEmail(trader.getEmail());
+        traderDTO.setFirstName(trader.getFirstName());
+        traderDTO.setLastName(trader.getFirstName());
+        traderDTO.setPassword(trader.getPassword());
+        traderDTO.setDateCreated(trader.getDateCreated());
+        return  traderDTO;
 
-    public String getB() {
-        return b;
     }
+    //MAPPING THE ENTITY TO THE DTO
+    public Trader mapToDTO(TraderDTO traderDTO){
+        Trader trader=new Trader();
+        trader.setId(traderDTO.getId());
+        trader.setEmail(traderDTO.getEmail());
+        trader.setFirstName(traderDTO.getFirstName());
+        trader.setLastName(traderDTO.getFirstName());
+        trader.setPassword(traderDTO.getPassword());
+        trader.setDateCreated(traderDTO.getDateCreated());
+        return  trader;
 
-    public void setB(String b) {
-        this.b = b;
     }
 }
